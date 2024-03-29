703db4d6d772e1d5243adf6e57c2c7f842f1edfe
teivah <teiva.harsanyi@gmail.com>
2024-03-29 17:54:35 +0100

    Simple bus implementation

--- proc/comp/bus.go
+++ proc/comp/bus.go
@@ -97,3 +97,51 @@ func (bus *BufferedBus[T]) Connect(currentCycle int) {
 	}
 	bus.buffer = bus.buffer[i:]
 }
+
+type SimpleBus[T any] struct {
+	pending entry[T]
+	current entry[T]
+}
+
+type entry[T any] struct {
+	exists bool
+	t      T
+}
+
+func (b *SimpleBus[T]) Flush() {
+	b.pending = entry[T]{}
+	b.current = entry[T]{}
+}
+
+func (b *SimpleBus[T]) Get() (t T, exists bool) {
+	if b.current.exists {
+		t = b.current.t
+		exists = true
+	}
+	b.current = b.pending
+	b.pending = entry[T]{}
+	return
+}
+
+func (b *SimpleBus[T]) Peek() (t T, res bool) {
+	if b.current.exists {
+		t = b.current.t
+		res = true
+	}
+	return
+}
+
+func (b *SimpleBus[T]) CanAdd() bool {
+	return !b.pending.exists
+}
+
+func (b *SimpleBus[T]) Add(t T) {
+	b.pending = entry[T]{
+		exists: true,
+		t:      t,
+	}
+}
+
+func (b *SimpleBus[T]) IsEmpty() bool {
+	return !b.pending.exists && !b.current.exists
+}
---
+++ proc/comp/bus_test.go
@@ -0,0 +1,49 @@
+package comp_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/teivah/majorana/proc/comp"
+)
+
+func TestSimpleBus(t *testing.T) {
+	b := &comp.SimpleBus[int]{}
+	var val int
+	var exists bool
+
+	val, exists = b.Peek()
+	busAssert(t, 0, false, val, exists)
+	val, exists = b.Get()
+	busAssert(t, 0, false, val, exists)
+
+	assert.Equal(t, true, b.CanAdd())
+	b.Add(1)
+	val, exists = b.Peek()
+	busAssert(t, 0, false, val, exists)
+	val, exists = b.Get()
+	busAssert(t, 0, false, val, exists)
+	val, exists = b.Get()
+	busAssert(t, 1, true, val, exists)
+	val, exists = b.Get()
+	busAssert(t, 0, false, val, exists)
+
+	assert.Equal(t, true, b.CanAdd())
+	b.Add(1)
+	assert.Equal(t, false, b.CanAdd())
+	val, exists = b.Get()
+	busAssert(t, 0, false, val, exists)
+	assert.Equal(t, true, b.CanAdd())
+	b.Add(2)
+	val, exists = b.Get()
+	busAssert(t, 1, true, val, exists)
+	val, exists = b.Get()
+	busAssert(t, 2, true, val, exists)
+	val, exists = b.Get()
+	busAssert(t, 0, false, val, exists)
+}
+
+func busAssert(t *testing.T, expectedVal int, expectedExists bool, val int, exists bool) {
+	assert.Equal(t, expectedVal, val)
+	assert.Equal(t, expectedExists, exists)
+}
