ae6343c1b9bd786f5ac66b5c966a9320d41f0e54
Teiva Harsanyi <teiva.harsanyi@gmail.com>
2024-04-03 19:57:51 +0200

    Detect newly data hazard

--- proc/mvp6/cu.go
+++ proc/mvp6/cu.go
@@ -1,6 +1,8 @@
 package mvp6
 
 import (
+	"fmt"
+
 	"github.com/teivah/majorana/common/log"
 	"github.com/teivah/majorana/common/obs"
 	"github.com/teivah/majorana/proc/comp"
@@ -53,6 +55,14 @@ func (u *controlUnit) cycle(cycle int, ctx *risc.Context) {
 	}
 
 	remaining := u.outBus.RemainingToAdd()
+	var pushedRunners []risc.InstructionRunner
+
+	defer func() {
+		for _, runner := range pushedRunners {
+			ctx.AddPendingRegisters(runner)
+		}
+	}()
+
 	for elem := range u.pendings.Iterator() {
 		pending := u.pendings.Value(elem)
 		if pushed > 0 && pending.Runner.InstructionType().IsBranch() {
@@ -62,8 +72,16 @@ func (u *controlUnit) cycle(cycle int, ctx *risc.Context) {
 
 		hazard, reason := ctx.IsDataHazard(pending.Runner)
 		if !hazard {
+			hazard, reason := isHazardWithPushedRunners(pushedRunners, pending.Runner)
+			if hazard {
+				log.Infoi(ctx, "CU", pending.Runner.InstructionType(), pending.Pc, "new data hazard: reason=%s", reason)
+				u.blockedDataHazard++
+				return
+			}
+
 			u.pushRunner(ctx, cycle, pending)
 			u.pendings.Remove(elem)
+			pushedRunners = append(pushedRunners, pending.Runner)
 			remaining--
 			pushed++
 		} else {
@@ -86,7 +104,16 @@ func (u *controlUnit) cycle(cycle int, ctx *risc.Context) {
 
 		hazard, reason := ctx.IsDataHazard(runner.Runner)
 		if !hazard {
+			hazard, reason := isHazardWithPushedRunners(pushedRunners, runner.Runner)
+			if hazard {
+				u.pendings.Push(runner)
+				log.Infoi(ctx, "CU", runner.Runner.InstructionType(), runner.Pc, "new data hazard: reason=%s", reason)
+				u.blockedDataHazard++
+				return
+			}
+
 			u.pushRunner(ctx, cycle, runner)
+			pushedRunners = append(pushedRunners, runner.Runner)
 			remaining--
 			pushed++
 		} else {
@@ -98,9 +125,31 @@ func (u *controlUnit) cycle(cycle int, ctx *risc.Context) {
 	}
 }
 
+func isHazardWithPushedRunners(pushedRunners []risc.InstructionRunner, runner risc.InstructionRunner) (bool, string) {
+	pendingWriteRegisters := make(map[risc.RegisterType]int)
+	for _, runner := range pushedRunners {
+		for _, register := range runner.WriteRegisters() {
+			if register == risc.Zero {
+				continue
+			}
+			pendingWriteRegisters[register]++
+		}
+	}
+
+	for _, register := range runner.ReadRegisters() {
+		if register == risc.Zero {
+			continue
+		}
+		if v, exists := pendingWriteRegisters[register]; exists && v > 0 {
+			// An instruction needs to read from a register that was updated
+			return true, fmt.Sprintf("Read hazard on %s", register)
+		}
+	}
+	return false, ""
+}
+
 func (u *controlUnit) pushRunner(ctx *risc.Context, cycle int, runner risc.InstructionRunnerPc) {
 	u.outBus.Add(runner, cycle)
-	ctx.AddPendingRegisters(runner.Runner)
 	log.Infoi(ctx, "CU", runner.Runner.InstructionType(), runner.Pc, "pushing runner")
 }
 
